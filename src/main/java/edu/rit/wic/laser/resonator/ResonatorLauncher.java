66509e1c19f24f6169899cd152270c065d95f113
Matthew Crocco <matthewcrocco@gmail.com>
2016-10-11 01:40:58 -0400

    Initial Commit

---
+++ src/main/java/edu/rit/wic/laser/resonator/ResonatorLauncher.java
@@ -0,0 +1,14 @@
+package edu.rit.wic.laser.resonator;
+
+import spark.Spark;
+
+/**
+ * Created by Matthew on 10/11/2016.
+ */
+public class ResonatorLauncher {
+
+	public static void main(String[] args) {
+
+	}
+
+}
